c051befc9c05a5770a004b035b230a50f48eba6e
DanielGonchar <dannygonchar@gmail.com>
2018-02-26 13:45:55 +0300

    First Task commit

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -13,8 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("ViewController: viewDidLoad\n")
+        
+        
+        view.backgroundColor = .red
+        
+        
     }
-
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("ViewController: viewWillAppear\n")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("ViewController: viewDidAppear\n")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("ViewController: viewWillLayoutSubviews\n")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("ViewController: viewDidLayoutSubviews\n")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("ViewController: viewWillDisappear\n")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("ViewController: viewDidDisappear\n")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
